0f5bec93c88eac79d36f79c8e1155c1757f685fc
Zach Smith <zachsmith.sa@gmail.com>
2019-09-13 17:14:15 +0200

    got a cumbersome working example of jsx going

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import React from 'https://dev.jspm.io/react'
+import babel from 'https://dev.jspm.io/@babel/core'
+import jsx from 'https://dev.jspm.io/@babel/plugin-transform-react-jsx'
 
-export default () => React.createElement('div', null, 'hello world')
+const appString = '() => <div>hello world</div>'
+export default eval(babel.transform(appString, {
+  plugins: [jsx]
+}).code)
